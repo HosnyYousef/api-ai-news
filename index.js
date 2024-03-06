8c740bf2265766054e072e02682f5eb7b457aee7
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-03-06 19:54:44 +0200

    got rid of text in packages.json scripts

---
+++ index.js
@@ -0,0 +1,14 @@
+const POST = 8000
+
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+// initalizing using express
+// we save it as app (variable) so we can use it further on, use as we please
+// we use crud with it app.
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+//this is what we need to write to get a message telling us everything is running fine
+
